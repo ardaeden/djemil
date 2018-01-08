42a79fba9e11c1ee46279ef2e074eb50ba931748
Arda Eden <ardaeden@gmail.com>
2018-01-08 22:16:45 +0300

    updates

--- main.c
+++ main.c
@@ -11,29 +11,34 @@ static t_class *djemil_class;
 typedef struct _djemil {
     t_object    x_obj;
     t_outlet    *f_out;
-    t_float     freq, scl;
+    t_float     freq, sclNo;
     t_int       fret;
 }t_djemil;
 
+//
+//void djemil_setMaqam(t_djemil *x, t_floatarg f) {
+//
+//    switch((t_int) f) {
+//        case 1:
+//            post("1st case triggered");
+//            x->scl = 0;
+//            break;
+//        case 2:
+//            post("2nd case triggrered");
+//            x->scl = 1;
+//            break;
+//    }
+//
+//}
 
-void djemil_set(t_djemil *x, t_floatarg f) {
-
-    switch((t_int) f) {
-        case 1:
-            post("1st case triggered");
-            x->scl = 0;
-            break;
-        case 2:
-            post("2nd case triggrered");
-            x->scl = 1;
-            break;
-    }
-
+void djemil_setMaqam(t_djemil *x, t_symbol *s, t_int argc, t_atom *argv) {
+    if (argv[0].a_type != A_SYMBOL) return;
+    //post("String = %s", atom_getsymbol(argv)->s_name);
 }
 
 void djemil_process(t_djemil *x, t_float f){
             x->fret = (t_int) f % 12;
-            x->freq = mtof(f)*scales[(t_int)x->scl][(t_int)x->fret];
+            x->freq = mtof(f)*scales[(t_int)x->sclNo][(t_int)x->fret];
             post("Note = %d", x->fret);
             outlet_float(x->x_obj.ob_outlet, x->freq);
 }
@@ -58,8 +63,10 @@ void djemil_setup(void) {
 
     class_addfloat(djemil_class, (t_method) djemil_process);
 
-    class_addmethod(djemil_class,
-                    (t_method)djemil_set,
-                    gensym("set"), A_DEFFLOAT, 0);
-
+//    class_addmethod(djemil_class,
+//                    (t_method) djemil_setMaqam,
+//                    gensym("set"), A_GIMME, 0);
+//    class_addlist(djemil_class, (t_method) djemil_setMaqam);
+    class_addmethod(djemil_class, (t_method) djemil_setMaqam,
+                    gensym("set"), A_GIMME, 0);
 }
